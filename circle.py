906f941a66b6c2dc3eb8327ca219a4f722f03c84
Felix Kjellberg <felix.kjellberg@gmail.com>
2025-10-29 09:33:06 +0100

    started with circle added circumference

---
+++ circle.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    @property
+    def radius(self):
+        return self._radius
+    
+    @radius.setter
+    def radius(self, value):
+        self._radius = value
+    
+    @property
+    def circumference(self) -> float:
+        return 2*self.radius*np.pi
