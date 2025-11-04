ce7849b4907de60517a16dd3ccca5eaebe647641
FellanNokes <felix.l.kjellberg@gmail.com>
2025-11-04 12:49:43 +0100

    added documentation to shape.py

--- shape.py
+++ shape.py
@@ -1,8 +1,29 @@
 import utils as utl
-#TODO: maybe we add operators here to be able to compare different shapes
-#TODO: Add dunder repr and str
 class Shape():
+    """
+    A class representing a shape that holds a 2 dimensional posistion
+
+    Attributes:
+    - x (float): x position of the shape
+    - y (float): y position of the shape
+
+    Nethods:
+    - translate(): adds the new x and y to the current position
+
+    Example usage:
+    >>> shape1 = Shape(1,3)
+    >>> shape1.translate(2,7)
+    >>> print(shape1)
+    A shape located at (x:3, y:10)
+    """
     def __init__(self, x, y):
+        """
+        Initializes a new instance of the Shape class.
+
+        Parameters:
+        - x (float): x position of the shape
+        - y (float): y position of the shape
+        """
         utl.validate_numbers(x,y)
         self._x = x
         self._y = y
@@ -14,9 +35,17 @@ class Shape():
     @property
     def y(self):
         return self._y
+    
+    def __repr__(self):
+        return f"Shape(x = {self.x},y = {self.y})"
+    
+    def __str__(self):
+        return f"A shape located at (x:{self.x}, y:{self.y})"
 
     #TODO: add validation 
     def translate(self, x, y):
+        """Adds a new position to the current position"""
+        utl.validate_numbers(x,y)
         self._x += x
         self._y += y
 
